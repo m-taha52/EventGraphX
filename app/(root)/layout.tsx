7f886251e394424f04e3e006fe45b6d4ceba2a01
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-18 12:47:11 +0530

    Update layout.tsx
    I removed the html because its not required. the div contains h-screen so it covers the entire page and flex-col so the items appear on top of each other

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Footer from "@/components/shared/Footer"
+import Header from "@/components/shared/Header"
+
 export default function RootLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
     return (
-      <html lang="en">
+      <div className="flex h-screen flex-col">
+        <Header />
         <body>{children}</body>
-      </html>
+        <Footer />
+      </div>
     )
   }
