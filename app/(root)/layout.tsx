7672f92c8000e6981035f9af3e72bee1e85d0152
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-18 20:51:06 +0530

    Update layout.tsx
    Added a comment

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     return (
       <div className="flex h-screen flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1">{children}</main> { /* The flex-1 property here allows the main to take up the remaining space in the container */}
         <Footer />
       </div>
     )
