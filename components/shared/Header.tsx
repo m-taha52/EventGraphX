16dad2869d62ce3529ddbfa581633b4510b82380
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-19 14:21:52 +0530

    Update Header.tsx
    The Link component below the div below Header is used to display the logo
    The Link makes it clickable to return back to the webpage.
    
    The div below the Link has a justify end to position the login button at the end of the navbar.
    
    This div contains our login buttons and profile button for logging out

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,34 @@
+import { SignedOut, UserButton, SignedIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header className='w-full border-b '>
+ <div className='wrapper flex items-center justify-between'>      {/* styles for wrapper found in globals.css  */}
+      <Link href="/" className='w-36'> 
+        <Image src="/assets/images/logo.png" width={128} height={38}
+        alt="EventGraphX"
+        />
+      </Link>
+
+
+    <div className='flex w-32 justify-end gap-3'>
+      <SignedIn>
+        <UserButton afterSignOutUrl='/' />
+      </SignedIn>
+      <SignedOut>
+        <Button asChild className='rounded-full' size='lg'>
+            <Link href="/sign-in">
+              Login
+            </Link>
+        </Button>
+      </SignedOut>
+    </div>
+  </div>
+   </header>
   )
 }
 
