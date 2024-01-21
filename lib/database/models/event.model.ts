5f4c6069b5c1fc38a0936bb0d1d37f9d5d39227b
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-21 12:13:44 +0530

    Create event.model.ts
    Created a schema model for our event and an interface. The purpose of the interface is to allow us to reference the schema at any time while writing the frontend

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,39 @@
+import { Document, model, Schema, models } from "mongoose";
+
+export interface IEvent extends Document {
+
+    _id: string;
+    title: string;
+    description?: string;
+    location?: string;
+    createdAt: Date;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    price?: string;
+    isFree: boolean;
+    url?: string;
+    category?: {_id: string, name: string}; // Assuming Category has a string representation as well
+    organizer?: {_id: string, firstName: string, lastName: string}  // Assuming User has a string representation as well
+
+}
+
+const EventSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String},
+    location: {type: String},
+    createdAt: {type: Date, default: Date.now},
+    imageUrl: {type: String, required: true},
+    startDateTime: {type: Date, required: true},
+    endDateTime: {type:Date, default: Date.now},
+    price: {type: String},
+    isFree: {type: Boolean, default: false},
+    url: {type:String},
+    category: {type: Schema.Types.ObjectId, ref: "Category"},
+    organizer: {type: Schema.Types.ObjectId, ref: "User"},
+
+})
+
+const Event = models.Event || model('Event', EventSchema)
+
+export default Event;
