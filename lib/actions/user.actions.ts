63d2c443590818ce0710c33d970efcfeb6c5b4d2
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-22 12:26:38 +0530

    Update user.actions.ts
    . "Implemented user update functionality using MongoDB's `findOneAndUpdate` method. The code retrieves a user document based on the `clerkId` and updates it with new data provided in the `user` object. The updated document is then returned and stored in the `updatedUser` variable for further processing."

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { CreateUserParams } from "@/types"
+import { CreateUserParams, UpdateUserParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
@@ -13,6 +13,24 @@ export const createUser = async (user: CreateUserParams) =>
 
         const newUser = await User.create(user);
         return JSON.parse(JSON.stringify(newUser));
+    } 
+    catch(error)
+    {
+        handleError(error)
+    }
+}
+
+export const updateUser = async (clerkId: String, user: UpdateUserParams) =>
+{
+    try 
+    {
+        await connectToDatabase();
+
+        const updatedUser = await User.findOneAndUpdate({clerkId}, user, {new: true});
+
+        if(!updateUser)
+            throw new Error("User Update Failed")
+        return JSON.parse(JSON.stringify(updateUser));
     } 
     catch(error)
     {
