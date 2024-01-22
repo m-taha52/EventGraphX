a3d2f9c9e8eeb811c1d5518045eeab1b008346f9
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-22 19:39:37 +0530

    Update user.actions.ts
    Created the deleted user method. Given that every user is also referenced in orders and events, we need to ensure to delink those relationships too
    
    "Update 'events' and 'orders' collections to remove references to userToDelete:
    
    - Remove userToDelete._id from the 'organizer' array in documents where the _id is in userToDelete.events in the 'events' collection.
    - Remove the 'buyer' field from documents where the _id is in userToDelete.orders in the 'orders' collection."

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Order from "../database/models/order.model"
 import Event from "../database/models/event.model"
+import { revalidatePath } from "next/cache"
 
 export const createUser = async (user: CreateUserParams) =>
 {
@@ -57,3 +58,36 @@ export const getUserById = async (userId: String) =>
         handleError(error)
     }
 }
+
+export async function deleteUser(clerkId: string) {
+    try {
+      await connectToDatabase()
+  
+      // Find user to delete
+      const userToDelete = await User.findOne({ clerkId })
+  
+      if (!userToDelete) {
+        throw new Error('User not found')
+      }
+  
+      // Unlink relationships
+      await Promise.all([
+        // Update the 'events' collection to remove references to the user
+        Event.updateMany(
+          { _id: { $in: userToDelete.events } },
+          { $pull: { organizer: userToDelete._id } }
+        ),
+  
+        // Update the 'orders' collection to remove references to the user
+        Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+      ])
+  
+      // Delete user
+      const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+      revalidatePath('/')
+  
+      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    } catch (error) {
+      handleError(error)
+    }
+  }
