693c30ea3f36cab3e1ed6d2d5b975fd0fcf9438c
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-25 14:16:45 +0530

    Update utils.ts
    Created a utility function that converts a file into a URL

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,5 @@ export const handleError = (error: unknown) => {
   console.error(error)
   throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
 }
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
