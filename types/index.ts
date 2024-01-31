7752aa5c8fca468e8c8ce6432e9984402a7ff557
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-01-31 11:54:14 +0530

    Update index.ts
    Added createEventParams

--- types/index.ts
+++ types/index.ts
@@ -20,3 +20,20 @@ export type CreateUserParams = {
 export type CreateCategoryParams = {
   categoryName: string
 }
+
+export type CreateEventParams = {
+  userId: string
+  event: {
+    title: string
+    description: string
+    location: string
+    imageUrl: string
+    startDateTime: Date
+    endDateTime: Date
+    categoryId: string
+    price: string
+    isFree: boolean
+    url: string
+  }
+  path: string
+}
