abdeaded92cc4c7de3d47dc3a00bed27a463b29f
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-11 21:50:05 +0530

    Update app\(root)\events\[id]\update\page.tsx
    Defined the UpdateEventProps which is just an id with a string value.
    Then we are fetching the event using the GetEventByID method. The event is then passed to the EventForm as props.
    We are also using clerk's sessionClaims to get the userId.
    This userId is used to verify if this user is authorized to make the update in the Eventform

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,8 +1,18 @@
 import EventForm from '@/components/shared/EventForm'
+import { getEventById } from '@/lib/actions/event.actions';
+import { UpdateEventParams } from '@/types';
 import { auth } from '@clerk/nextjs';
 import React from 'react'
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({ params: {id}}: UpdateEventProps) => {
+
+    const event = await getEventById(id);
 
     const { sessionClaims } = auth();
 
@@ -14,7 +24,11 @@ const UpdateEvent = () => {
         </section>
 
         <div className='wrapper my-8'>
-            <EventForm userId={userId} type="Update" />
+            <EventForm 
+            userId={userId} 
+            event={event} 
+            eventId={event._id} 
+            type="Update" />
         </div>
      </>
   )
