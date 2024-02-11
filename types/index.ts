3a7bca99905e095129aa2317d3d25a745f5a67d2
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-11 21:50:53 +0530

    Update types\index.ts
    Defined the types for the UpdateEventParams

--- types/index.ts
+++ types/index.ts
@@ -54,3 +54,21 @@ export type DeleteEventParams = {
   eventId: string
   path: string
 }
+
+export type UpdateEventParams = {
+  userId: string
+  event: {
+    _id: string
+    title: string
+    imageUrl: string
+    description: string
+    location: string
+    startDateTime: Date
+    endDateTime: Date
+    categoryId: string
+    price: string
+    isFree: boolean
+    url: string
+  }
+  path: string
+}
