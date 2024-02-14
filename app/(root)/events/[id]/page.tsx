ad8a3f2c3da9eeb21de29f281624d1a30d34116d
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-14 19:02:35 +0530

    Update app\(root)\events\[id]\page.tsx
    Added the checkout button to the event details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
@@ -51,6 +52,7 @@ const EventDetails = async ({params: {id}, searchParams}: SearchParamProps) => {
             </div>
 
             {/* Checkout Button */}
+            <CheckoutButton event={event} />
 
             <div className='flex flex-col gap-5'>
               <div className='flex gap-2 md:gap-3'>
