69a944046c2d112550df5462837edb90905f137e
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-14 09:51:17 +0530

    Update lib\actions\event.actions.ts
    Created a new event action called getRelatedEventsByCategory. This event filters for events that are created by the same user or are from the same category.

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,5 +1,5 @@
 'use server'
-import { CreateEventParams, DeleteEventParams, GetAllEventsParams, UpdateEventParams } from "@/types"
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams, GetRelatedEventsByCategoryParams, UpdateEventParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
@@ -122,6 +122,32 @@ export async function updateEvent({ userId, event, path }: UpdateEventParams) {
       revalidatePath(path)
   
       return JSON.parse(JSON.stringify(updatedEvent))
+    } catch (error) {
+      handleError(error)
+    }
+  }
+
+  export async function getRelatedEventsByCategory({
+    categoryId,
+    eventId,
+    limit = 3,
+    page = 1,
+  }: GetRelatedEventsByCategoryParams) {
+    try {
+      await connectToDatabase()
+  
+      const skipAmount = (Number(page) - 1) * limit
+      const conditions = { $and: [{ category: categoryId }, { _id: { $ne: eventId } }] }
+  
+      const eventsQuery = Event.find(conditions)
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(limit)
+  
+      const events = await populateEvent(eventsQuery)
+      const eventsCount = await Event.countDocuments(conditions)
+  
+      return { data: JSON.parse(JSON.stringify(events)), totalPages: Math.ceil(eventsCount / limit) }
     } catch (error) {
       handleError(error)
     }
