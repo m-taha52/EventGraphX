a20c579ef7e7f0aa14bbc0e874f3e01e4954a735
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-15 02:38:37 +0530

    Update lib\actions\order.actions.ts
    Created a new order action that will take the order information from stripe and send it to the MongoDB database.

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,10 @@
 'use server'
 import Stripe  from "stripe";
-import { CheckoutOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams } from "@/types"
 import { redirect } from 'next/navigation';
+import { connectToDatabase } from "../database";
+import { handleError } from "../utils";
+import Order from "../database/models/order.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) =>
 {
@@ -35,4 +38,20 @@ export const checkoutOrder = async (order: CheckoutOrderParams) =>
     } catch(error){
         throw error;
     }
+}
+
+export const createOrder = async (order: CreateOrderParams) => {
+  try {
+    await connectToDatabase();
+    
+    const newOrder = await Order.create({
+      ...order,
+      event: order.eventId,
+      buyer: order.buyerId,
+    });
+
+    return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {
+    handleError(error);
+  }
 }
