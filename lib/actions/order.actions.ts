c952488b99a4587fb2a87b4fd913becf4818a30f
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-15 09:02:46 +0530

    Update order.actions.ts
    Written a complex query to fetch the users who have purchased each event

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,6 +7,8 @@ import { handleError } from "../utils";
 import Order from "../database/models/order.model";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
+import { ObjectId } from "mongodb";
+
 
 export const checkoutOrder = async (order: CheckoutOrderParams) =>
 {
@@ -83,6 +85,62 @@ export async function getOrdersByUser({ userId, limit = 3, page }: GetOrdersByUs
     const ordersCount = await Order.distinct('event._id').countDocuments(conditions)
 
     return { data: JSON.parse(JSON.stringify(orders)), totalPages: Math.ceil(ordersCount / limit) }
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+export async function getOrdersByEvent({ searchString, eventId }: GetOrdersByEventParams) {
+  try {
+    await connectToDatabase()
+
+    if (!eventId) throw new Error('Event ID is required')
+    const eventObjectId = new ObjectId(eventId)
+
+    const orders = await Order.aggregate([
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'buyer',
+          foreignField: '_id',
+          as: 'buyer',
+        },
+      },
+      {
+        $unwind: '$buyer',
+      },
+      {
+        $lookup: {
+          from: 'events',
+          localField: 'event',
+          foreignField: '_id',
+          as: 'event',
+        },
+      },
+      {
+        $unwind: '$event',
+      },
+      {
+        $project: {
+          _id: 1,
+          totalAmount: 1,
+          createdAt: 1,
+          eventTitle: '$event.title',
+          eventId: '$event._id',
+          buyer: {
+            $concat: ['$buyer.firstName', ' ', '$buyer.lastName'],
+          },
+        },
+      },
+      {
+        $match: {
+          $and: [{ eventId: eventObjectId }, { buyer: { $regex: RegExp(searchString, 'i') } }],
+        },
+      },
+    ])
+
+    return JSON.parse(JSON.stringify(orders))
   } catch (error) {
     handleError(error)
   }
