c2c6f34c0bf4aa401f2a7bb5f60eead96c6c7992
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-15 16:37:52 +0530

    Update utils.ts
    Created a new utility function that passes our search query into a mongodb query to fetch data from the DB

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
+import { UrlQueryParams } from "@/types"
 import { type ClassValue, clsx } from "clsx"
+import qs from 'query-string'
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -55,4 +57,18 @@ export const formatPrice = (price: string) => {
   }).format(amount)
 
   return formattedPrice
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
 }
