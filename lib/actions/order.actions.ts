4c0ea2d58d6f1d2f01a070938af44d17ebe3f7be
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-15 09:00:19 +0530

    Update lib\actions\order.actions.ts
    Created the order action to fetch the events purchased by each user

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,10 +1,12 @@
 'use server'
 import Stripe  from "stripe";
-import { CheckoutOrderParams, CreateOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams, GetOrdersByUserParams } from "@/types"
 import { redirect } from 'next/navigation';
 import { connectToDatabase } from "../database";
 import { handleError } from "../utils";
 import Order from "../database/models/order.model";
+import Event from "../database/models/event.model";
+import User from "../database/models/user.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) =>
 {
@@ -53,5 +55,35 @@ export const createOrder = async (order: CreateOrderParams) => {
     return JSON.parse(JSON.stringify(newOrder));
   } catch (error) {
     handleError(error);
+  }
+}
+
+export async function getOrdersByUser({ userId, limit = 3, page }: GetOrdersByUserParams) {
+  try {
+    await connectToDatabase()
+
+    const skipAmount = (Number(page) - 1) * limit
+    const conditions = { buyer: userId }
+
+    const orders = await Order.distinct('event._id')
+      .find(conditions)
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(limit)
+      .populate({
+        path: 'event',
+        model: Event,
+        populate: {
+          path: 'organizer',
+          model: User,
+          select: '_id firstName lastName',
+        },
+      })
+
+    const ordersCount = await Order.distinct('event._id').countDocuments(conditions)
+
+    return { data: JSON.parse(JSON.stringify(orders)), totalPages: Math.ceil(ordersCount / limit) }
+  } catch (error) {
+    handleError(error)
   }
 }
