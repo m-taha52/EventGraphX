efe39e2253f70786a9516acdf8af6447f44be125
Mohammed Taha Alibhai <mohammedt243@gmail.com>
2024-02-15 10:43:52 +0530

    Update lib\utils.ts
    Created a new utility function to format price

--- lib/utils.ts
+++ lib/utils.ts
@@ -46,3 +46,13 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   }
 }
+
+export const formatPrice = (price: string) => {
+  const amount = parseFloat(price)
+  const formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+
+  return formattedPrice
+}
